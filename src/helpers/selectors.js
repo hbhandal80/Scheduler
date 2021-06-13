b7583798551e31338c1dacdaa58e70166c57ad82
Hervinder Bhandal <hervinderbhandal@Hervinders-MacBook-Pro.local>
2021-06-12 23:03:27 -0700

    Added interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,9 +12,20 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  }
+  const interviewObj = {
+    "student": interview.student,
+    "interviewer": {
+      "id": state.interviewers[interview.interviewer].id,
+      "name": state.interviewers[interview.interviewer].name,
+      "avatar": state.interviewers[interview.interviewer].avatar
+    }
+  }
+  return interviewObj;
 
-};
+}
 
 export function getInterviewersForDay(state, day) {
- 
 };
